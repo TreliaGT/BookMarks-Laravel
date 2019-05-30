6735a30a46a9b50ba830c44d05c69c2461343949
Trelia <42791605+TreliaGT@users.noreply.github.com>
2019-05-30 11:06:59 +0800

    updated welcome screen

--- resources/views/Pages/welcome.blade.php
+++ resources/views/Pages/welcome.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1> hi</h1>
-
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center">
+                <div class="card-header ">
+                    <h2> welcome to the BookMark application</h2>
+                </div>
+                <div class="card-body">
+                    <p> Please Register or Login to Continue</p>
+                    <p> The Register and Login is located up on the left side of the window.</p>
+                </div>
+                <div class="card-footer">
+                    <span> Made By Georgia Trinidad v244682 2019</span>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
