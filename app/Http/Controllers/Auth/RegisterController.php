f66ff26b846ee95c0f349e8573f9bf3cb3094f03
TreliaGT <v244682@tafe.wa.edu.au>
2019-06-24 11:00:36 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-          'email' => ['required', 'string', 'email', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'FirstName' => ['Max:128', 'string'],
             'LastName' => ['Max:128', 'string'],
